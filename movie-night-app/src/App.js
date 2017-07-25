ca766159ca43f10098facd6d4c2d50946a65fac4
tkopp <tkopp@pixar.com>
2017-07-24 23:25:06 -0700

    poster and text alignment

--- movie-night-app/src/App.js
+++ movie-night-app/src/App.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 import fetch from 'isomorphic-fetch'
 import { Grid, Row, Col, Image, Modal, Button, Table} from 'react-bootstrap';
-
-
+import questionMark from './icons/question-mark.jpg'
 
 class SearchBar extends Component {
   constructor(props) {
     super(props);
-    this.state = {"movie" : "ok"};
-    this.state.movies = ['']
+    this.state = {
+      movies : [{original_title: '', id:'0'}]
+    };
+
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
@@ -20,38 +21,70 @@ class SearchBar extends Component {
   }
 
   handleSubmit(event){
-    fetch("http://localhost:3000/search?q=" + this.state.value, {
-      method: "GET",
-      headers: { 'Content-Type': 'application/json' }
-    }).then((response) => {
-      return response.json();
-    }).then((parsedData) => {
-      this.setState({movies:parsedData.results})
-      console.log(this.state.movies[0].id);
-    })
+    if(this.state.value !== ''){
+      fetch("http://localhost:3000/search?q=" + this.state.value, {
+        method: "GET",
+        headers: { 'Content-Type': 'application/json' }
+      }).then((response) => {
+        return response.json();
+      }).then((parsedData) => {
+        this.setState({movies: parsedData.results})
+        console.log(parsedData);
+      })
+    }
   }
 
   render() {
+    const movies = this.state.movies
+
     return (
       <div>
         <input type="text" value={this.state.value || ''} placeholder='Enter Movie Title Here' onChange={this.handleChange} />
         <button onClick={this.handleSubmit}>Submit</button>
-        {this.state.movies.map((movie) => {
-          return (
-            <Table responsive>
-              <tbody>
-                <tr><td key={movie.id}>{movie.original_title}</td></tr>
-              </tbody>
-            </Table>
-
-          )
-        })}
+        <Grid>
+          {movies.map((movie, index) => {
+            if( movie.original_title && movie.original_title !== '' && movie.poster_path){
+              return (
+                  <Row key={movie.id} className="show-grid">
+                      <Col xs={3} md={3} xsOffset={3}>
+                        <Image src={"https://image.tmdb.org/t/p/w300" + movie.poster_path} alt={movie.original_title} responsive />
+                      </Col>
+                      <Col xs={6} md={6} className="content">
+                        <h3 className="text-left">{movie.original_title}</h3>
+                        <p className="text-left">{movie.overview}</p>
+                      </Col>
+                      <Col xs={12} md={12} className="spacer-row"></Col>
+                  </Row>
+              )
+            }
+            else if( movie.original_title && movie.original_title !== '' && !movie.poster_path){
+                return (
+                  <Row key={movie.id} className="show-grid">
+                    <Col xs={3} md={3} xsOffset={3}>
+                      <Image src ={questionMark} alt="?" className="missing"  />
+                      <Col xs={12} md={12}>
+                        <h4 >Image Missing</h4>
+                      </Col>
+                    </Col>
+                    <Col xs={6} md={6} className="content">
+                      <h3 className="text-left">{movie.original_title}</h3>
+                      <p className="text-left">{movie.overview}</p>
+                    </Col>
+                  </Row>
+              )
+            }
+            else{
+              return(
+              <Row className="show-grid" key={index}></Row>
+              )
+            }
+          })}
+        </Grid>
       </div>
-
     )
   }
-
 }
+
 
 class App extends Component {
   render() {
